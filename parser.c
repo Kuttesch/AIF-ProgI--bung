3fcec2c0ab9f764c2dba8e7422ff630f3a8bc210
Kuttesch <moritzkuttesch@outlook.de>
2025-10-02 15:50:46 +0200

    Better Parser version

--- parser.c
+++ parser.c
@@ -1,71 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-int get_file_content(char *filename, char *buffer);
-int write_file_content(char *filename, char *buffer);
-int parse(char *buffer);
+int parse(FILE *in, FILE *out);
 bool check_char(char source, char comp);
 bool is_lower(char *character);
 void to_upper(char *character);
 
 int main() {
-    char *buffer;
-    get_file_content((char *){"file.txt"}, buffer);
-    parse(buffer);
-    write_file_content((char *){"file.txt"}, buffer);
+    FILE *in, *out;
+    char filename[256];
+    char outname[300];
+
+    printf("Please provide Filename: ");
+    scanf("%255s", filename);
+    char *dot = strrchr(filename, '.');
+    if (dot) {
+        size_t len = dot - filename;
+        strncpy(outname, filename, len);
+        outname[len] = '\0';
+        strcat(outname, "_out");
+        strcat(outname, dot);
+    } else {
+        strcpy(outname, filename);
+        strcat(outname, "_out.txt");
+    }
+
+    if ((in = fopen(filename, "r")) != NULL &&
+        (out = fopen(outname, "w")) != NULL) {
+
+        parse(in, out); 
+
+        fclose(in);
+        fclose(out);
+    } else {
+        perror("");
+    }
     return 0;
 }
 
-int get_file_content(char *filename, char *buffer) {
-    FILE *file;
-    if ((file = fopen(filename, "r")) != NULL) {
-        fseek(file, 0, SEEK_END);
-        long size = ftell(file);
-        rewind(file);
-
-        buffer = malloc(size + 1);
-        fread(buffer, 1, size, file);
-        buffer[-1] = 0x00;
-
-        // !TODO
-        fclose(file);
-    } else {return -1;}
-}
-
-int write_file_content(char *filename, char *buffer) {
-    FILE *file;
-    if ((file = fopen(filename, "w")) != NULL) {
-        fprintf(file, buffer);
-        fclose(file);
-    }
-}
-
-int parse(char *buffer) {
-    bool EOL = false;
-    while (!EOL) {
-        if (check_char(*buffer, 0x00)) {
-            EOL = true;
-            break;
-        }
-        to_upper(buffer);
-        if (!check_char(*buffer, 0x20 /* SPACE */)) {
-            buffer++;
+int parse(FILE *in, FILE *out) {
+    char character;
+    bool EOL;
+    while ((character = fgetc(in)) && !EOL) {
+        if (check_char(character, EOF)) {EOL = true; break;}
+        to_upper(&character);
+        if (!check_char(character, 0x20 /* SPACE */)) {
+            fputc(character, out);
         }
     }
-    *buffer = 0x00;
+    return 0;
 }
 
 bool check_char(char source, char comp) {
-    return (bool)(source == comp);
+    return (source == comp);
 }
 
 bool is_lower(char* character) {
-    return (bool)(97 < *character && *character < 122);
+    return (*character >= 97 && *character <= 122);
 }
 
 void to_upper(char *character) {
     if (is_lower(character)) {
-        *character = *character + 32;
+        *character = *character - 32;
     }
 }
